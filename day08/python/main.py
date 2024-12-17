b1cb570e8835a26e83a386c28bd1e1158ca7edf2
Abraxas <p.buciuni@protonmail.com>
2024-12-17 07:41:01 +0100

    day08: Added functions to find antinodes coords and to check if in grid

--- day08/python/main.py
+++ day08/python/main.py
@@ -14,3 +14,38 @@ for el in content:
 
 for line in antennasCoords:
     print(line)
+
+
+def inGrid(coords):
+    global maxLineIndex
+    global content
+    xMax = len(content[0])
+    yMax = maxLineIndex
+    if (coords[0] < 0 or coords[0] > xMax):
+        return False
+    if (coords[1] < 0 or coords[1] > yMax):
+        return False
+    return True
+
+
+def findAntinodesCoords(a, b):
+    yVecDistance = b[1] - a[1]
+    xVecDistance = b[0] - a[0]
+    firstAntinode, secondAntinode = set(), set()
+    firstAntinode = (b[0] + xVecDistance, b[1] + yVecDistance)
+    secondAntinode = (a[0] - xVecDistance, a[1] - yVecDistance)
+    # if xVecDistance < 0:
+    #     firstAntinode = (b[0] + xVecDistance, b[1] + yVecDistance)
+    # else:
+    #     firstAntinode = (b[0] - xVecDistance, b[1] + yVecDistance)
+    # if xVecDistance > 0:
+    #     firstAntinode = (b[0] - xVecDistance, b[1] - yVecDistance)
+    # else:
+    #     firstAntinode = (b[0] + xVecDistance, b[1] - yVecDistance)
+    return [firstAntinode, secondAntinode]
+
+
+print("In grid antenna: ", inGrid(antennasCoords[3][1]))
+print("In grid antenna: ", inGrid((16, 9)))
+result = findAntinodesCoords(antennasCoords[0][1], antennasCoords[1][1])
+print(result)
