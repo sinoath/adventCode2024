0bba2c06b2daad1cc882c44987373c7c6ff903b6
Abraxas <p.buciuni@protonmail.com>
2024-12-24 15:04:37 +0100

    day11: defining functions to solve the puzzle
    mySplit, insertSplitStone and multi2024

--- day11/python/main.py
+++ day11/python/main.py
@@ -5,4 +5,50 @@ for line in f:
 f.close()
 
 stones = content.split(" ")
-print(stones)
+
+
+def mySplit(char):
+    lenght = len(char)
+    firstStone = char[:(lenght // 2)]
+    tempStone = char[(lenght // 2):]
+    while tempStone[0] == "0":
+        if len(tempStone) > 1:
+            tempStone = tempStone[1:]
+        else:
+            break
+    return [firstStone, tempStone]
+
+
+def insertSplitStone(index, stones, splittedStones):
+    stones[index] = splittedStones[1]
+    stones.insert(index, splittedStones[0])
+
+
+def multi2024(char):
+    i = int(char)
+    i *= 2024
+    return str(i)
+    pass
+
+
+# testChar = "1013102030"
+# result = mySplit(testChar)
+
+# randomList = [str(i) for i in range(6)]
+# print(randomList)
+# index = 2
+# # randomList[index] = result[1]
+# # randomList.insert(index, result[0])
+# insertSplitStone(index, randomList, result)
+# print(randomList)
+# print(multi2024("2"))
+# # print(testChar)
+# # print(result)
+
+
+def main():
+    print("test")
+
+
+if __name__ == "__main__":
+    main()
