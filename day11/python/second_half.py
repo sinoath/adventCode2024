6319fca52f721c730d8bfeed602890ba4fae8e72
Abraxas <p.buciuni@protonmail.com>
2024-12-26 08:44:54 +0100

    day11: deleted comments and useless, commented code

--- day11/python/second_half.py
+++ day11/python/second_half.py
@@ -6,9 +6,6 @@ f.close()
 
 NUMBER_OF_BLINKS = 25
 LARGER_NUM_OF_BLINKS = 75
-# stones = content.split(" ")
-# dictStones = {key: 1 for key in stones}
-# print(dictStones)
 
 
 def mySplit(char):
@@ -34,7 +31,6 @@ def insertSplitStone(stones, myDict):
 def insertStone(stone, myDict, value=1):
     if stone in myDict.keys():
         myDict[stone] += value
-        # myDict[stone] += myDict[stone]
     else:
         myDict[stone] = value
 
@@ -48,10 +44,8 @@ def multi2024(char):
 def main():
 
     BLINKS = 101
-    # cycle = 1
     stones = content.split(" ")
     dictStones = {key: 1 for key in stones}
-    # print(dictStones)
     for _ in range(BLINKS):
 
         toBeSplit = dict()
@@ -68,9 +62,6 @@ def main():
                 toBeMulti[k] = v
 
         dictStones = dict()
-        # print("Split: ", toBeSplit)
-        # print("Multi: ", toBeMulti)
-        # print("Zeros: ", zeroToOne)
 
         # Adding stones with inscription 0
         if zeroToOne != {}:
@@ -81,38 +72,16 @@ def main():
             stones = mySplit(key)
             insertStone(stones[0], dictStones, value)
             insertStone(stones[1], dictStones, value)
-            # dictStones[stones[0]] = value
-            # dictStones[stones[1]] = value
 
         # Adding stones with multiplied inscription
         for key, value in toBeMulti.items():
             new_key = multi2024(key)
             dictStones[new_key] = value
 
-        # ones = 0
-        # if "1" in dictStones.keys():
-        #     ones += dictStones["1"]
-        # if zeroToOne != {}:
-        #     ones += zeroToOne["0"]
-        # if ones > 0:
-        #     dictStones["1"] = ones
-
-        # print()
-        # print("After ", cycle, " blinks")
-        # for k, v in dictStones.items():
-        #     print(k, " ", v)
-        # cycle += 1
-        # print("After ", cycle, " blinks")
-        # print(dictStones)
-        # print()
-        # cycle += 1
-
     numberOfStones = 0
     for el in dictStones:
         numberOfStones += dictStones[el]
     print(numberOfStones)
-    # after 25 cycles the answer is correct 193899
-    # after 75 cycles, 229682156298283 is not the correct answer
 
 
 if __name__ == "__main__":
