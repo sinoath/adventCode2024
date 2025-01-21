0e1cc525e7863cedb7e8466e336f1a7cc50a3ddd
Abraxas <p.buciuni@protonmail.com>
2025-01-21 13:31:09 +0100

    day13: defined buttonValues and prizeCoords functions
    parsing input strings, the functions return button values and prize
    coordinates as integers

--- day13/python/main.py
+++ day13/python/main.py
@@ -16,6 +16,29 @@ for line in content:
         clawMachine = []
 
 
+def buttonValues(clawMach):
+    '''Return button values for a given claw machine'''
+    splitted = clawMach.split(",")
+    print(splitted)
+    left = splitted[0].split("+")
+    right = splitted[1].split("+")
+    x = int(left[1])
+    y = int(right[1])
+    return [x, y]
+
+
+def prizeCoords(string):
+    '''Return the prize coordinates
+    Input: string with the X and Y coordinates
+    Output: a list with the X and Y integers'''
+    splitted = string.split(",")
+    left = splitted[0].split("=")
+    right = splitted[1].split("=")
+    x, y = int(left[1]), int(right[1])
+    return [x, y]
+    pass
+
+
 print(listOfMachines)
 print()
 for el in listOfMachines:
